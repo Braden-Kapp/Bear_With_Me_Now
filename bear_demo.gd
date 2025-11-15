9edd45c48d2ad4c69db09d15041bbf5b0a9c54eb
Braden-Kapp <braden.h.kapp@gmail.com>
2025-11-15 01:25:32 -0600

    Most major mechanics have been implemented

--- bear_demo.gd
+++ bear_demo.gd
@@ -1,25 +1,71 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const MAX_JUMP_VELOCITY = -1250.0
-
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+const SPEED = 800.0
+const JUMP_VELOCITY = -1300.0
+const MAX_JUMP_VELOCITY = -1800.0
+var in_grab_area = false
+var grabbin = false
+var grab_left = false
+var grab_right = false
 
 func _physics_process(delta: float) -> void:
+	
+	in_grab_area = grab_left or grab_right
+	if in_grab_area:
+		_grab()
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		if not grabbin:
+			velocity += get_gravity() * 2.0 * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jumpPanda") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("leftPanda", "rightPanda")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+	if not grabbin:
+		var direction := Input.get_axis("leftPanda", "rightPanda")
+		if direction:
+			velocity.x = direction * SPEED
+			if Input.is_action_pressed("rightPanda"):
+				if is_on_floor():
+					animated_sprite.play("rightPanda")
+				else:
+					animated_sprite.play("rightJumpPanda")
+			if Input.is_action_pressed("leftPanda"):
+				if is_on_floor():
+					animated_sprite.play("leftPanda")
+				else:
+					animated_sprite.play("leftJumpPanda")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			animated_sprite.play("standPanda")
+			
 	
 	move_and_slide()
+
+	
+
+func _on_left_grab_area_2d_area_entered(area: Area2D) -> void:
+	grab_left = true
+
+func _on_right_grab_area_2d_area_entered(area: Area2D) -> void:
+	grab_right = true
+	
+func _on_left_grab_area_2d_area_exited(area: Area2D) -> void:
+	grab_left = false
+
+func _on_right_grab_area_2d_area_exited(area: Area2D) -> void:
+	grab_right = false
+	#change sprites for grab func
+func _grab():
+	if Input.is_action_pressed("grabPanda"):
+		grabbin = true
+		velocity = Vector2.ZERO  # stop movement completely
+		
+		if grab_right:
+			animated_sprite.play("standPanda")
+		elif grab_left:
+			animated_sprite.play("standPanda")
+	if Input.is_action_just_released("grabPanda"):
+		grabbin = false
--- scenes/polar_bear_demo.gd
+++ scenes/polar_bear_demo.gd
@@ -1,14 +1,20 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const MAX_JUMP_VELOCITY = -1250.0
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 800.0
+const JUMP_VELOCITY = -1300.0
+const MAX_JUMP_VELOCITY = -2500.0
+var bigJumpin = false
 
 
 func _physics_process(delta: float) -> void:
+	if is_on_floor():
+		bigJumpin = false
+	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * 2.0 * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jumpPolar") and is_on_floor():
@@ -18,13 +24,31 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("leftPolar", "rightPolar")
 	if direction:
 		velocity.x = direction * SPEED
+		if(not bigJumpin):
+			if Input.is_action_pressed("rightPolar"):
+				if is_on_floor():
+					animated_sprite.play("rightPolar")
+				else:
+					animated_sprite.play("rightJumpPolar")
+			if Input.is_action_pressed("leftPolar"):
+				if is_on_floor():
+					animated_sprite.play("leftPolar")
+				else:
+					animated_sprite.play("leftJumpPolar")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if(not bigJumpin):
+			animated_sprite.play("standingPolar")
+
 		
 	if is_on_floor():
 		_large_jump(delta)
 	
 	move_and_slide()
 func _large_jump(delta: float):
+	if Input.is_action_pressed("bigjumpPolar"):
+		animated_sprite.play("bigJumpPolar")
 	if Input.is_action_just_released("bigjumpPolar"):
 		velocity.y =  MAX_JUMP_VELOCITY
+		bigJumpin = true
+		animated_sprite.play("whileBigJump")
